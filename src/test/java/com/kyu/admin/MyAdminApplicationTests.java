30d1c0a629292ca1892a3c882f5a6b6e53ade9ac
nklee <lng1982@gmail.com>
2021-05-15 21:37:06 +0900

    init

---
+++ src/test/java/com/kyu/admin/MyAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyu.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
